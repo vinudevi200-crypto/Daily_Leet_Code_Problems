c370ee6a8b116be7a3b05c551ae5015b3ed7e6b6
Vinushika M <vinudevi200@gmail.com>
2026-08-05 22:00:38 +0530

    Delete Day-1_BuildArrayFromPermutation directory

--- Day-1_BuildArrayFromPermutation/BuildArrayFromPermutationSolution.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] buildArray(int[] nums) {
-        int[] ans=new int[nums.length];
-        for(int i;i<nums.length;i++)  {
-            ans[i]nums[nums[i]];
-        } 
-        return ans;
-         }
-}
