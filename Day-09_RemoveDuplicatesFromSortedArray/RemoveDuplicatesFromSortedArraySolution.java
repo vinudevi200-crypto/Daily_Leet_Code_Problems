c5c7aa72a429c236b185d38424c6c6c44b82e3c6
Vinushika M <vinudevi200@gmail.com>
2026-08-05 22:42:34 +0530

    Implement removeDuplicates method in Solution class

--- Day-09_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArraySolution.java
+++ Day-09_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArraySolution.java
@@ -1,2 +1,13 @@
-
-RemoveDuplicatesFromSortedArray
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j=0;
+        for (int i=1;i<nums.length;i++){
+            if(nums[j]!=nums[i]){
+                j++;
+                nums[j]=nums[i];
+                
+            }
+        }
+        return j+1;
+    }
+}
